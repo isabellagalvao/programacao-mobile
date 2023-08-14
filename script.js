5a4bf7fca6c855ffd554d0a7681e6f10edf5c116
isabellagalvao <128341357+isabellagalvao@users.noreply.github.com>
2023-08-14 14:19:38 -0300

    contagem inteligente

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-function tabuada() {
+function contagem() { 
     let saida = document.getElementById('saida') 
-    let n = Number(document.getElementById('fnum').value) 
-    saida.innerHTML = `<h2>Tabuada de ${n}</h2>` 
-    let c = 1 
-    while (c <= 10) { 
-     saida.innerHTML += `${n} x ${c} = <strong>${n * c}</strong><br>` 
-    c ++ 
+    let n1 = Number(document.getElementById('fn1').value) 
+    let n2 = Number(document.getElementById('fn2').value) 
+    let cont 
+    
+    saida.innerHTML += `<h2>Contando de ${n1} até ${n2} </h2>`
+     if (n1<n2) {
+    cont = n1
+    while (cont <= n2) {
+    saida.innerHTML += ` ${cont} &#x1F449;`
+    cont ++
+    } 
+     } else if (n2<n1) {
+    cont = n1
+     while (cont >= n2) { 
+     saida.innerHTML += ` ${cont} &#x1F449;` 
+     cont --
     }
+     } else {
+    saida.innerHTML += `Não é possível contar, pois os números são iguais`
+    }
+    
+    saida.innerHTML += ` &#x1F3C1;`
     }
